2ac2ef4d3ffe3ddbeb50c575a26c4516ebb6c762
EmilKivi <emil.kivi.gbg22@amerikanskagymnasiet.se>
2022-11-23 23:11:25 +0100

    Documentation,about, comments and fixes
    Ive added documentation and about us to my website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*All code in here is copied*/
+/*The first code is copied*/
 
 
 
@@ -23,11 +23,26 @@
         }
       }
 
+
+
+
+//This code is NOT copied
+
+    
   let container = document.querySelectorAll('.add')
 
-
+  var cartnumber = 0;
   for (let i=0; i < container.length; i++ ) {
+    //When you click on one of the buttons that has the class add, a function is executed that changes the number at the cart image to the number of times you clicked
+
     container[i].addEventListener('click',()=>{
       console.log("added to cart");
-     })
-  }
+      cartnumber++
+      console.log(cartnumber);
+      var cartNumberText = String(cartnumber)
+      // Changes the contents of the tags with the id cartnumber to the value in cartNumberText
+      document.getElementById("cartnumber").innerHTML = cartNumberText;
+     });
+  };
+  
+  
---
+++ slideshow.js
@@ -0,0 +1,30 @@
+// Make an array with each image to be included in the slideshow
+var imgArray = [
+    './image/Racing-Glove.png',
+    './image/Racing-suit.png',
+    './image/Racing-Glove3.png',
+    './image/Racing_helmet1.png',
+    './image/Racing-Glove2.png',
+    './image/racing-helmet2.png',
+    './image/Racing-Suit2.png',
+    './image/Racing-suit3.png',
+    './image/racing-helmet3.png',
+
+];
+var curIndex = 0;
+//How long an image should be displayed, each 1000 is 1 second
+var imgDuration = 3000;
+
+function slideShow() {
+    //The IMG taken with the id image1 will be equal to the object in the imgArray that has the position that curIndex specifies
+    document.getElementById('image1').src = imgArray[curIndex];
+    curIndex++;
+    //If curIndex becomes as long as the amount of objects in imgArray, curIndex restarts from the beginning so that the slide show restarts
+
+    if (curIndex == imgArray.length) { curIndex = 0; }
+    //Restarts the function after a certain time that is in imgDuration and causes a new image to appear
+
+    setTimeout("slideShow()", imgDuration);
+}
+//starts the slideshow
+slideShow();
